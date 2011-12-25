f9e28e7ddca50e35598591bacd3f5b1c03d489ee
Luca Spiller <luca@stackednotion.com>
2011-12-25 21:36:33 +0000

    Change spec module to use execution tree

--- src/espec.erl
+++ src/espec.erl
@@ -30,108 +30,44 @@ run_spec(Mod, Spec) ->
     run_spec(Mod, Spec, espec_console_listener:new(), espec_console_listener).
 
 run_spec(Mod, Spec, ListenerState0, ListenerModule) ->
+    ExecutionTree = espec_ast:convert_to_execution_tree(Spec),
     ListenerState1 = ListenerModule:start_spec(Mod, ListenerState0),
-    ListenerState2 = lists:foldl(fun({Description, Children}, GroupListenerState) ->
-          run_group(GroupListenerState, ListenerModule, Description, [], [], Children)
-    end, ListenerState1, extract_groups(Spec)),
+    ListenerState2 = run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok),
     ListenerModule:end_spec(Mod, ListenerState2).
 
-run_group(ListenerState0, ListenerModule, GroupDescription, Befores, Afters, Children) ->
+run_execution_tree([], ListenerState0, _, _) ->
+    ListenerState0;
+run_execution_tree([{start_group, _Line, GroupDescription} | ExecutionTree], ListenerState0, ListenerModule, _) ->
     ListenerState1 = ListenerModule:start_group(GroupDescription, ListenerState0),
-    BeforeEach = Befores ++ extract_before_each(Children),
-    AfterEach = Afters ++ extract_after_each(Children),
-    run_before(extract_before_all(Children)),
-    ListenerState2 = lists:foldl(fun
-            ({example, _Line, Description, Fun}, ExampleListenerState0) ->
-                ExampleListenerState1 = ListenerModule:start_example(Description, ExampleListenerState0),
-                Result = run_test(Fun, BeforeEach, AfterEach),
-                ListenerModule:end_example(Description, Result, ExampleListenerState1);
-            ({pending, _Line, Description}, ExampleListenerState0) ->
-                ListenerModule:pending_example(Description, ExampleListenerState0)
-    end, ListenerState1, extract_tests(Children)),
+    run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok);
+run_execution_tree([{end_group, _Line, GroupDescription} | ExecutionTree], ListenerState0, ListenerModule, _) ->
+    ListenerState1 = ListenerModule:end_group(GroupDescription, ListenerState0),
+    run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok);
 
-    ListenerState3 = lists:foldl(fun({Description, SubChildren}, GroupListenerState0) ->
-        run_group(GroupListenerState0, ListenerModule, Description, BeforeEach, AfterEach, SubChildren)
-    end, ListenerState2, extract_groups(Children)),
-    run_after(extract_after_all(Children)),
-    ListenerModule:end_group(GroupDescription, ListenerState3).
+run_execution_tree([{pending_example, _Line, ExampleDescription} | ExecutionTree], ListenerState0, ListenerModule, _) ->
+    ListenerState1 = ListenerModule:pending_example(ExampleDescription, ListenerState0),
+    run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok);
+run_execution_tree([{start_example, _Line, ExampleDescription} | ExecutionTree], ListenerState0, ListenerModule, _) ->
+    ListenerState1 = ListenerModule:start_example(ExampleDescription, ListenerState0),
+    run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok);
+run_execution_tree([{end_example, _Line, ExampleDescription} | ExecutionTree], ListenerState0, ListenerModule, Result) ->
+    ListenerState1 = ListenerModule:end_example(ExampleDescription, Result, ListenerState0),
+    run_execution_tree(ExecutionTree, ListenerState1, ListenerModule, ok);
 
-extract_before_all([]) ->
-    [];
-extract_before_all([{before_, _Line, all, Fun} | _]) ->
-    [Fun];
-extract_before_all([_ | Rem]) ->
-    extract_before_all(Rem).
+run_execution_tree([{run, Fun} | ExecutionTree], ListenerState0, ListenerModule, ok) ->
+    Result = execute_test(Fun),
+    run_execution_tree(ExecutionTree, ListenerState0, ListenerModule, Result);
+run_execution_tree([{run, _} | ExecutionTree], ListenerState0, ListenerModule, Error) ->
+    % Don't run a fun if there are existing errors
+    run_execution_tree(ExecutionTree, ListenerState0, ListenerModule, Error).
 
-extract_after_all([]) ->
-    [];
-extract_after_all([{after_, _Line, all, Fun} | _]) ->
-    [Fun];
-extract_after_all([_ | Rem]) ->
-    extract_after_all(Rem).
-
-extract_before_each([]) ->
-    [];
-extract_before_each([{before_, _Line, each, Fun} | _]) ->
-    [Fun];
-extract_before_each([_ | Rem]) ->
-    extract_before_each(Rem).
-
-extract_after_each([]) ->
-    [];
-extract_after_each([{after_, _Line, each, Fun} | _]) ->
-    [Fun];
-extract_after_each([_ | Rem]) ->
-    extract_after_each(Rem).
-
-extract_tests(Tests) ->
-    extract_tests(Tests, []).
-extract_tests([], Matched) ->
-    lists:reverse(Matched);
-extract_tests([{example, Line, Description, Fun} | Rem], Matched) ->
-    extract_tests(Rem, [{example, Line, Description, Fun} | Matched]);
-extract_tests([{pending, Line, Description} | Rem], Matched) ->
-    extract_tests(Rem, [{pending, Line, Description} | Matched]);
-extract_tests([_ | Rem], Matched) ->
-    extract_tests(Rem, Matched).
-
-extract_groups(Groups) ->
-    extract_groups(Groups, []).
-extract_groups([], Matched) ->
-    lists:reverse(Matched);
-extract_groups([{group, _Line, Description, Body} | Rem], Matched) ->
-    extract_groups(Rem, [{Description, Body} | Matched]);
-extract_groups([_ | Rem], Matched) ->
-    extract_groups(Rem, Matched).
-
-run_test(Fun, Before, After) ->
+execute_test(Fun) ->
     try
-        run_before(Before),
         Fun(),
-        run_after(After),
         ok
     catch
-        throw:{before_failed, {Class, Reason}} ->
-            {error, {before_failed, {Class, Reason}}};
-        throw:{after_failed, {Class, Reason}} ->
-            {error, {after_failed, {Class, Reason}}};
         Class:Reason ->
             {error, {Class, Reason}}
-    end.
-
-run_before(Funs) ->
-    run_functions(before_failed, Funs).
-
-run_after(Funs) ->
-    run_functions(after_failed, lists:reverse(Funs)).
-
-
-run_functions(Error, Funs) ->
-    try
-        lists:map(fun(F) -> F() end, Funs)
-    catch
-        Class:Reason ->
-            throw({Error, {Class, Reason}})
     end.
 
 is_setup(Part) ->
