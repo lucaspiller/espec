ab12e635445a896d77d6de141f0ed97eb2c40c87
Luca Spiller <luca@stackednotion.com>
2011-12-25 19:27:03 +0000

    Add specs for error handling

---
+++ spec/error_handling_spec.erl
@@ -0,0 +1,51 @@
+-module(error_handling_spec).
+-include("espec.hrl").
+
+spec() ->
+  describe("badmatch errors", fun() ->
+        it("should be caught and returned", fun() ->
+              State = espec:run_spec(error_example_spec, badmatch_error_example_spec(), espec_null_listener:new(), espec_null_listener),
+              {error, {error, {badmatch,false}}} = proplists:get_value("should fail", State)
+          end)
+    end),
+
+  describe("throws", fun() ->
+        it("should be caught and returned", fun() ->
+              State = espec:run_spec(error_example_spec, throw_error_example_spec(), espec_null_listener:new(), espec_null_listener),
+              {error, {throw, something_went_wrong}} = proplists:get_value("should fail", State)
+          end)
+    end),
+
+  describe("exits", fun() ->
+        it("should be caught and returned", fun() ->
+              State = espec:run_spec(error_example_spec, exit_error_example_spec(), espec_null_listener:new(), espec_null_listener),
+              {error, {exit, goodbye}} = proplists:get_value("should fail", State)
+          end)
+    end).
+
+%
+% Example specs for testing
+%
+
+badmatch_error_example_spec() ->
+  describe("error spec", fun() ->
+      it("should fail", fun() ->
+            A = true,
+            B = false,
+            A = B
+      end)
+  end).
+
+throw_error_example_spec() ->
+  describe("error spec", fun() ->
+      it("should fail", fun() ->
+            throw(something_went_wrong)
+      end)
+  end).
+
+exit_error_example_spec() ->
+  describe("error spec", fun() ->
+      it("should fail", fun() ->
+            exit(goodbye)
+      end)
+  end).
