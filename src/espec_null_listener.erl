058552a7d8c64150fde4e7052524fba3d3d47b19
Luca Spiller <luca@stackednotion.com>
2011-12-25 19:26:54 +0000

    Null listener should return result of examples ran

--- src/espec_null_listener.erl
+++ src/espec_null_listener.erl
@@ -2,10 +2,8 @@
 -behaviour(espec_listener).
 -export([start_spec/2,end_spec/2,start_example/2, end_example/3, pending_example/2, start_group/2, end_group/2, new/0]).
 
-
-
 new() ->
-  null_listener.
+  [].
 
 start_spec(_Name, State) ->
   State.
@@ -16,15 +14,14 @@ end_spec(_Name, State) ->
 start_example(_Description, State) ->
   State.
 
-end_example(_Description, _Result, State) ->
-  State.
+end_example(Description, Result, State) ->
+  [{Description, Result} | State].
 
-pending_example(_Description, State) ->
-  State.
+pending_example(Description, State) ->
+  [{Description, pending} | State].
 
 start_group(_GroupDescription, State) ->
   State.
-
 
 end_group(_Description, State) ->
   State.
