5980652f1fadc4262a6f6481ce7ea77664de195e
Luca Spiller <luca@stackednotion.com>
2011-12-25 21:13:01 +0000

    Include filters in execution tree

--- src/espec_ast.erl
+++ src/espec_ast.erl
@@ -8,7 +8,9 @@ convert_to_execution_tree(AST) ->
 
 convert_to_execution_tree(AST, []) ->
   {Filters, ASTWithoutFilters} = extract_filters_at_current_level(AST, [], []),
-  convert_to_execution_tree(ASTWithoutFilters, Filters, []).
+  Parsed = [proplists:get_value(before_all, Filters, [])],
+  ExecutionTree = convert_to_execution_tree(ASTWithoutFilters, Filters, Parsed),
+  lists:flatten([proplists:get_value(after_all, Filters, []) | ExecutionTree]).
 
 convert_to_execution_tree([], _, Parsed) ->
   lists:flatten(Parsed);
@@ -21,9 +23,13 @@ convert_to_execution_tree([{group, Line, Description, Body} | AST], Filters, Par
   ],
   convert_to_execution_tree(AST, Filters, [InstructionTree | Parsed]);
 convert_to_execution_tree([{example, Line, Description, Fun} | AST], Filters, Parsed) ->
+  BeforeInstruction = proplists:get_value(before_each, Filters, []),
+  AfterInstruction = proplists:get_value(after_each, Filters, []),
   InstructionTree = [
     {end_example, Line, Description},
+    AfterInstruction,
     {run, Fun},
+    BeforeInstruction,
     {start_example, Line, Description}
   ],
   convert_to_execution_tree(AST, Filters, [InstructionTree | Parsed]);
@@ -36,24 +42,24 @@ convert_to_execution_tree([{pending, Line, Description} | AST], Filters, Parsed)
 extract_filters_at_current_level([], Filters, Parsed) ->
   {Filters, lists:reverse(Parsed)};
 extract_filters_at_current_level([{before_, Line, each, Fun} | AST], Filters, Parsed) ->
-  extract_filters_at_current_level(AST, [
-      {before_each, Line, Fun}
-      | Filters
-    ], Parsed);
+  InstructionTree = [
+    {run, Fun}
+  ],
+  extract_filters_at_current_level(AST, [{before_each, InstructionTree} | Filters], Parsed);
 extract_filters_at_current_level([{after_, Line, each, Fun} | AST], Filters, Parsed) ->
-  extract_filters_at_current_level(AST, [
-      {after_each, Line, Fun}
-      | Filters
-    ], Parsed);
+  InstructionTree = [
+    {run, Fun}
+  ],
+  extract_filters_at_current_level(AST, [{after_each, InstructionTree} | Filters], Parsed);
 extract_filters_at_current_level([{before_, Line, all, Fun} | AST], Filters, Parsed) ->
-  extract_filters_at_current_level(AST, [
-      {before_all, Line, Fun}
-      | Filters
-    ], Parsed);
+  InstructionTree = [
+    {run, Fun}
+  ],
+  extract_filters_at_current_level(AST, [{before_all, InstructionTree} | Filters], Parsed);
 extract_filters_at_current_level([{after_, Line, all, Fun} | AST], Filters, Parsed) ->
-  extract_filters_at_current_level(AST, [
-      {after_all, Line, Fun}
-      | Filters
-    ], Parsed);
+  InstructionTree = [
+    {run, Fun}
+  ],
+  extract_filters_at_current_level(AST, [{after_all, InstructionTree} | Filters], Parsed);
 extract_filters_at_current_level([Instruction | AST], Filters, Parsed) ->
   extract_filters_at_current_level(AST, Filters, [Instruction | Parsed]).
