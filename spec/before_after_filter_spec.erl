214c85c99abb6c77716b3eca48f8ecd4c91e21b6
Ben Murphy <benmmurphy@gmail.com>
2011-12-26 13:02:50 +0000

    add specs for multiple before/after all/each

--- spec/before_after_filter_spec.erl
+++ spec/before_after_filter_spec.erl
@@ -21,7 +21,12 @@ spec() ->
         it("should run 'outer before' before each example in the nested group", fun() ->
               espec:run_spec(before_each_example_spec, before_each_example4_spec(), espec_null_listener:new(), espec_null_listener),
               [ran_before_each, example, ran_before_each, ran_before_each_nested, nested_example, ran_before_each, ran_before_each_nested, nested_example2] = get(before_each4)
-          end)
+          end),
+
+        it("should run multiple before each filters", fun() ->
+              espec:run_spec(before_each_multiple_before_filter_spec, before_each_multiple_before_filter_spec(), espec_null_listener:new(), espec_null_listener),
+              [ran_before_each, ran_before_each2, example] = get(before_each_multiple_before_filter)
+        end)
 
     end),
 
@@ -39,7 +44,14 @@ spec() ->
         it("should run after examples in current group and nested groups", fun() ->
               espec:run_spec(after_each_example_spec, after_each_example3_spec(), espec_null_listener:new(), espec_null_listener),
               [example, ran_after_each, nested_example, ran_after_each_nested, ran_after_each] = get(after_each3)
-          end)
+          end),
+
+        it("should run multiple after each filters", fun() ->
+              espec:run_spec(after_each_multiple_after_filter_spec, after_each_multiple_after_filter_spec(), espec_null_listener:new(), espec_null_listener),
+              [example, ran_after_each2, ran_after_each] = get(after_each_multiple_after_filter)
+        end)
+
+
     end),
 
   describe("before all filters", fun() ->
@@ -56,7 +68,13 @@ spec() ->
         it("should run before all once before all examples in the current group and nested groups", fun() ->
               espec:run_spec(before_all_example_spec, before_all_example3_spec(), espec_null_listener:new(), espec_null_listener),
               [ran_before_all, example, ran_before_all_nested, nested_example] = get(before_all3)
-          end)
+          end),
+
+        it("should run multiple before all filters", fun() ->
+              espec:run_spec(before_all_multiple_before_filter_spec, before_all_multiple_before_filter_spec(), espec_null_listener:new(), espec_null_listener),
+              [ran_before_all, ran_before_all2, example] = get(before_all_multiple_before_filter)
+        end)
+
 
 
     end),
@@ -75,7 +93,14 @@ spec() ->
         it("should run after all once after all examples in the current group and nested groups", fun() ->
               espec:run_spec(after_all_example_spec, after_all_example_spec(), espec_null_listener:new(), espec_null_listener),
               [example, nested_example, ran_after_all_nested, ran_after_all] = get(after_all3)
-          end)
+          end),
+
+        it("should run multiple after all filters", fun() ->
+              espec:run_spec(after_all_multiple_after_filter_spec, after_all_multiple_after_filter_spec(), espec_null_listener:new(), espec_null_listener),
+              [example, ran_after_all2, ran_after_all] = get(after_all_multiple_after_filter)
+        end)
+
+
     end).
 
 %
@@ -163,6 +188,71 @@ before_each_example4_spec() ->
               end)
           end)
     end).
+
+before_each_multiple_before_filter_spec() ->
+  describe("before_each spec", fun() ->
+        before_each(fun() ->
+              append(ran_before_each, before_each_multiple_before_filter)
+          end),
+
+        before_each(fun() ->
+              append(ran_before_each2, before_each_multiple_before_filter)
+        end),
+
+        it("should do stuff", fun() ->
+              append(example, before_each_multiple_before_filter)
+          end)
+
+    end).
+
+after_each_multiple_after_filter_spec() ->
+  describe("after_each spec", fun() ->
+        after_each(fun() ->
+              append(ran_after_each, after_each_multiple_after_filter)
+          end),
+
+        after_each(fun() ->
+              append(ran_after_each2, after_each_multiple_after_filter)
+        end),
+
+        it("should do stuff", fun() ->
+              append(example, after_each_multiple_after_filter)
+          end)
+
+    end).
+
+after_all_multiple_after_filter_spec() ->
+  describe("after_all spec", fun() ->
+        after_all(fun() ->
+              append(ran_after_all, after_all_multiple_after_filter)
+          end),
+
+        after_all(fun() ->
+              append(ran_after_all2, after_all_multiple_after_filter)
+        end),
+
+        it("should do stuff", fun() ->
+              append(example, after_all_multiple_after_filter)
+          end)
+
+    end).
+
+before_all_multiple_before_filter_spec() ->
+  describe("before_all spec", fun() ->
+        before_all(fun() ->
+              append(ran_before_all, before_all_multiple_before_filter)
+          end),
+
+        before_all(fun() ->
+              append(ran_before_all2, before_all_multiple_before_filter)
+        end),
+
+        it("should do stuff", fun() ->
+              append(example, before_all_multiple_before_filter)
+          end)
+
+    end).
+
 
 
 after_each_example1_spec() ->
